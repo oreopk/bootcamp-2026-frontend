c0de1aa6d3d922fb00e764527f27cb36a4b2643e
pkoreo <sart6zaV@yandex.ru>
2026-07-12 16:10:04 +0300

    feat: main functionality

--- launcher/game-launcher.js
+++ launcher/game-launcher.js
@@ -1,9 +1,27 @@
 class GameLauncher extends HTMLElement {
   constructor() {
     super();
+    this.revealTimer = null;
+    this.handleMessage = this.handleMessage.bind(this);
   }
 
   connectedCallback() {
+    const gameUrl = this.getAttribute('src');
+
+    if (!gameUrl) {
+      this.remove();
+      return;
+    }
+
+    let parsedGameUrl;
+
+    try {
+      parsedGameUrl = new URL(gameUrl, window.location.href);
+    } catch {
+      this.remove();
+      return;
+    }
+
     const shadow = this.attachShadow({ mode: 'open' });
 
     shadow.innerHTML = `
@@ -12,6 +30,28 @@ class GameLauncher extends HTMLElement {
         position: fixed;
         inset: 0;
         z-index: 999999;
+        background: #ffffff;
+      }
+
+      .loader {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 48px;
+        height: 48px;
+        border: 5px solid #bdbdbd;
+        border-top-color: #333333;
+        border-radius: 50%;
+        transform: translate(-50%, -50%);
+        animation: spin 0.8s linear infinite;
+      }
+
+      @keyframes spin {
+        to {
+          transform:
+            translate(-50%, -50%)
+            rotate(360deg);
+        }
       }
 
       iframe {
@@ -38,6 +78,40 @@ class GameLauncher extends HTMLElement {
 
     this.iframe = shadow.querySelector('iframe');
     this.loader = shadow.querySelector('.loader');
+
+    window.addEventListener('message', this.handleMessage);
+    this.iframe.src = parsedGameUrl.href;
+  }
+
+  handleMessage(event) {
+    if (event.source !== this.iframe?.contentWindow) {
+      return;
+    }
+
+    if (event.data?.type === 'GAME_READY') {
+      clearTimeout(this.revealTimer);
+
+      this.revealTimer = setTimeout(() => {
+        this.loader?.remove();
+        this.iframe?.classList.add('ready');
+
+        this.iframe?.focus();
+        this.iframe?.contentWindow?.focus();
+
+        this.revealTimer = null;
+      }, 500);
+
+      return;
+    }
+
+    if (event.data?.type === 'GAME_CLOSE') {
+      this.remove();
+    }
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('message', this.handleMessage);
+    clearTimeout(this.revealTimer);
   }
 }
 
